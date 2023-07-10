88a63951c6bead78d04e5d01f0b809bf23121f9c
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-10 21:04:23 +0500

    yahlitlash

--- basic_func12.py
+++ basic_func12.py
@@ -1,5 +1,8 @@
 def main(a):
-    '''Round the value of a to 2 decimal places and return it.
+    b=round(a,2)
+    return b
+print(main(6.453))
+'''Round the value of a to 2 decimal places and return it.
     
     Args:
         a (float): a number
@@ -7,4 +10,3 @@ def main(a):
     Returns:
         float: the result
     '''
-    return
